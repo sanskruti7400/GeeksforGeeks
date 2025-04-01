41357aaae88ac10b38a4e43c609c693190af85ba
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-04-01 21:51:22 +0530

    Create   Largestsubarraywith0sum.java

---
+++ Largestsubarraywith0sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    int maxLen(int arr[]) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+        int sum=0;
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            if(sum==0){
+                max=i+1;
+            }else{
+                if(map.containsKey(sum)){
+                    max=Math.max(max,i-map.get(sum));
+                }else{
+                    map.put(sum,i);
+                }
+            }
+        }
+        return max;
+    }
+}
