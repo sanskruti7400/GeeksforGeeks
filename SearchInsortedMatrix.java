a0a18dd702ad9e0f79a1d7921c5ca0bf9a2488d0
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-06-30 08:55:47 -0700

    Create SearchInsortedMatrix.java

---
+++ SearchInsortedMatrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                if(x==mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }    
+}
